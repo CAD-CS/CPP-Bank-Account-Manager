68de0d203525092353cc2521fdf312e2c3018e85
CAD-CS <parsa.hara117@gmail.com>
2025-07-22 15:49:40 -0700

    add gitignore

---
+++ include/Util.hpp
@@ -0,0 +1,33 @@
+#include "BankAccount.hpp"
+
+std::vector<BankAccount*> createAccounts() 
+{
+	std::ifstream reader("data/input.txt");
+
+	if (!reader.is_open()) 
+	{
+		std::cout << "Somthing went wrong with opening the file" << std::endl;
+	}
+
+	std::string firstName;
+	std::string lastName;
+	int initBalance;
+
+	std::vector<BankAccount*> vec_bankAccount;
+	BankAccount* p_temp;
+
+	while (reader >> firstName >> lastName >> initBalance)
+	{
+		p_temp = new BankAccount(firstName, lastName, initBalance);
+		p_temp->print();
+		vec_bankAccount.push_back(p_temp);
+
+	}
+	reader.close();
+	return vec_bankAccount;
+}
+
+void displayCommands() 
+{
+	std::cout << "| Exit (q) " << std::endl;
+}
